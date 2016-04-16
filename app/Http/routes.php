a892daaedff2e6ffbee41a060209594a71a915be
Francesco Malatesta <hellofrancesco@gmail.com>
2016-04-16 10:31:14 +0200

    Right naming for request items

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,8 @@
 <?php
 
 $app->get('webhook', function (\Illuminate\Http\Request $request) use ($app) {
-    if($request->get('verify_token') == env('VERIFY_TOKEN')) {
-        return $request->get('challenge');
+    if($request->get('hub_verify_token') == env('VERIFY_TOKEN')) {
+        return $request->get('hub_challenge');
     }
 
     return 'Wrong validation token';
