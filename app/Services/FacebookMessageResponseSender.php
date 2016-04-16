36c81a39501d00bc4a5dbf06cdc660927e0cd3a0
Francesco Malatesta <hellofrancesco@gmail.com>
2016-04-16 11:47:59 +0200

    Edit base_uri and uri

--- app/Services/FacebookMessageResponseSender.php
+++ app/Services/FacebookMessageResponseSender.php
@@ -9,11 +9,11 @@ class FacebookMessageResponseSender
 {
     public function sendQuote($recipientUserId, Quote $quote)
     {
-        $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/me/messages']);
+        $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
 
         $client->request(
             'POST',
-            '/',
+            'me/messages',
             [
                 'query' => ['access_token' => env('PAGE_ACCESS_TOKEN')],
                 'json' => [
