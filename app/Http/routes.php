5ed7009a4ef4e8dc4accac5166707b22f6028118
Francesco Malatesta <hellofrancesco@gmail.com>
2016-04-16 10:37:20 +0200

    First test with POST /webhook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+$app->post('webhook', function(\Illuminate\Http\Request $request) use ($app) {
+    file_put_contents('test.dat', json_encode($request->all()));
+});
+
 $app->get('webhook', function (\Illuminate\Http\Request $request) use ($app) {
     if($request->get('hub_verify_token') == env('VERIFY_TOKEN')) {
         return $request->get('hub_challenge');
