fb7be83f879dfb44382b736463587e25089c1c3e
Francesco Malatesta <hellofrancesco@gmail.com>
2016-04-18 21:19:39 +0200

    Add 10 quotes to the seeder

--- database/seeds/QuotesTableSeeder.php
+++ database/seeds/QuotesTableSeeder.php
@@ -12,13 +12,53 @@ class QuotesTableSeeder extends Seeder
     public function run()
     {
         app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
-            'Good, better, best. Never let it rest. \'Til your good is better and your better is best.',
-            'St. Jerome'
+            'Life is really simple, but we insist on making it complicated.',
+            'Confucius'
         ]);
 
         app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
-            'Life is 10% what happens to you and 90% how you react to it.',
-            'Charles R. Swindoll'
+            'The most important thing is to enjoy your life - to be happy - it\'s all that matters.',
+            'Audrey Hepburn'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.',
+            'Buddha'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'Life isn\'t about finding yourself. Life is about creating yourself.',
+            'George Bernard Shaw'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'We do not remember days, we remember moments.',
+            'Cesare Pavese'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'Very little is needed to make a happy life; it is all within yourself, in your way of thinking.',
+            'Marcus Aurelius'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'In the end, it\'s not the years in your life that count. It\'s the life in your years.',
+            'Abraham Lincoln'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'Life is not a problem to be solved, but a reality to be experienced.',
+            'Soren Kierkegaard'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'Keep your face to the sunshine and you cannot see a shadow.',
+            'Helen Keller'
+        ]);
+
+        app('db')->insert('insert into quotes (quote, author) values (?, ?)', [
+            'You can\'t blame gravity for falling in love.',
+            'Albert Einstein'
         ]);
     }
 }
